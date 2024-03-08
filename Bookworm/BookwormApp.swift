90419d50d13d8752de462fafb0b835269514062c
Monty Harper <monty@montyharper.com>
2024-03-08 11:57:03 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Monty Harper on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
